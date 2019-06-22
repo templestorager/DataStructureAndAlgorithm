e64ca86694d76ec01e6c175f3d3123e7311d479b
templestorager <templestorager@temple.edu>
2019-06-22 13:19:19 -0600

    Implement Str brute force solution

---
+++ Implement strStr.cpp
@@ -0,0 +1,16 @@
+// this is solution using brute force (O(nm), no proprocessing)
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        for( int i = 0; ; i++ ){
+            for( int j = 0; ; j++ ){
+                if( j == needle.size() ) 
+                    return i;
+                if ( i + j == haystack.size() )
+                    return -1;
+                if ( haystack[i+j] != needle[j] )
+                    break;
+            }
+        }
+    }
+};
